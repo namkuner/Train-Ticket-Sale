8c68de224eac6c7a639ca883159310525dcf2c3a
DungNguyen <nguyenvandung12092003@gmail.com>
2023-04-19 15:06:59 +0700

    abc

---
+++ src/views/HomePage/assets/js/validateForm.js
@@ -0,0 +1,41 @@
+const inputSearch = document.querySelector(".input-search")
+
+inputSearch.onkeyup = (e) => {
+    //console.log(e.target.value)
+    let checkData = e.target.value
+}
+
+let recomentlist = [
+    "Hà Nội",
+    "Phủ lý",
+    "Nam Định",
+    "Ninh Bình",
+    "Bỉm Sơn",
+    "Thanh Hóa",
+    "Minh Khôi",
+    "Chợ Sy",
+    "Vinh",
+    "Yên Trung",
+    "Hương Phố",
+    "Đồng Lê",
+    "Đồng Hới",
+    "Đông Hà",
+    "Huế",
+    "Đà Nẵng",
+    "Tam Kỳ",
+    "Núi Thành",
+    "Quãng Ngãi",
+    "Đức Phổ",
+    "Bồng Sơn",
+    "Diêu Trì",
+    "Tuy Hòa",
+    "Giã",
+    "Ninh Hòa",
+    "Nha Trang",
+    "Tháp Chàm",
+    "Sôn Mao",
+    "Bình Thuận",
+    "Biên Hòa",
+    "Dĩ An",
+    "Sài Gòn",
+]
