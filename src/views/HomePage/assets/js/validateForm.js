7dffe256d433da19acf63e59450038a982866ae4
DungNguyen <nguyenvandung12092003@gmail.com>
2023-04-19 16:33:23 +0700

    abc

--- src/views/HomePage/assets/js/validateForm.js
+++ src/views/HomePage/assets/js/validateForm.js
@@ -1,8 +1,40 @@
 const inputSearch = document.querySelector(".input-search")
+const autoBox = document.querySelector(".autobox")
 
 inputSearch.onkeyup = (e) => {
     //console.log(e.target.value)
     let checkData = e.target.value
+    let dataArray = []
+    if(checkData){
+        dataArray = recomentlist.filter((data) => {
+            return data.toLocaleLowerCase().startsWith(checkData.toLocaleLowerCase())
+        })
+        
+        dataArray = dataArray.map((data) => {
+            return data = '<li>'+data+'</li>'
+        })
+        autoBox.classList.add('active')
+        showAdress (dataArray)
+        let liItem = autoBox.querySelectorAll("li")
+        for(let i=0;i<liItem.length;i++){
+            liItem[i].addEventListener("click",function(){
+                inputSearch.value = liItem[i].innerHTML
+                autoBox.classList.remove('active')
+            })
+        }
+        //console.log(dataArray)
+    }else{
+        autoBox.classList.remove('active')
+    }
+}
+function showAdress (list){
+    let listData
+    if (!list.length) {
+        listData = '<li>'+inputSearch.value+'</li>'
+    }else {
+        listData = list.join('')
+    }
+    autoBox.innerHTML = listData
 }
 
 let recomentlist = [
