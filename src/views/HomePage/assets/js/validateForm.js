8365193835b1688b5d74793fb42e1a9527bc540c
DungNguyen <nguyenvandung12092003@gmail.com>
2023-04-20 09:44:36 +0700

    Tim kiem nha ga trong trang chu

--- src/views/HomePage/assets/js/validateForm.js
+++ src/views/HomePage/assets/js/validateForm.js
@@ -2,6 +2,9 @@ const inputSearch = document.querySelector(".input-search")
 const autoBox = document.querySelector(".autobox")
 const outputSearch = document.querySelector(".output-search")
 const autoBox2 = document.querySelector(".autobox2")
+const gatauSearch = document.querySelector(".gatau-search")
+const autoBox3 = document.querySelector(".autobox3")
+
 
 //Nơi đi
 inputSearch.onkeyup = (e) => {
@@ -113,3 +116,92 @@ function showAdress2 (list){
     autoBox2.innerHTML = listData2
 }
 
+//Tìm ga tàu
+
+gatauSearch.onkeyup = (e) => {
+    //console.log(e.target.value)
+    let checkData = e.target.value
+    let dataArray = []
+    if(checkData){
+        dataArray = recomentlist.filter((data) => {
+            return data.toLocaleLowerCase().startsWith(checkData.toLocaleLowerCase())
+        })
+        
+        dataArray = dataArray.map((data) => {
+            return data = '<li>'+data+'</li>'
+        })
+        autoBox3.classList.add('active')
+        showAdress3 (dataArray)
+        let liItem = autoBox3.querySelectorAll("li")
+        for(let i=0;i<liItem.length;i++){
+            liItem[i].addEventListener("click",function(){
+                gatauSearch.value = liItem[i].innerHTML
+                autoBox3.classList.remove('active')
+            })
+        }
+        //console.log(dataArray)
+    }else{
+        let allDataArray = recomentlist.map((data) => {
+            return data = '<li>'+data+'</li>'
+        })
+        autoBox3.classList.add('active')
+        showAdress3 (allDataArray)
+        let liItem = autoBox3.querySelectorAll("li")
+        for(let i=0;i<liItem.length;i++){
+            liItem[i].addEventListener("click",function(){
+                gatauSearch.value = liItem[i].innerHTML
+                autoBox3.classList.remove('active')
+            })
+        }
+    }
+}
+//
+gatauSearch.addEventListener("click", function(){
+    autoBox3.classList.remove('active');
+    let allDataArray = recomentlist.map((data) => {
+        return data = '<li>'+data+'</li>'
+    })
+    autoBox3.classList.add('active')
+    showAdress3 (allDataArray)
+    let liItem = autoBox3.querySelectorAll("li")
+    for(let i=0;i<liItem.length;i++){
+        liItem[i].addEventListener("click",function(){
+            gatauSearch.value = liItem[i].innerHTML
+            autoBox3.classList.remove('active')
+        })
+    }
+})
+
+//
+function showAdress3 (list){
+    let listData3
+    if (!list.length) {
+        listData3 = '<li>'+gatauSearch.value+'</li>'
+    }else {
+        listData3 = list.join('')
+    }
+    autoBox3.innerHTML = listData3
+    //show các ga tàu khi click vào
+    gatauSearch.addEventListener("click", function() {
+        document.querySelector(".table").classList.add("active");
+    });
+}
+//Khi ckick ra khỏi ô tìm ga tàu thì bảng dữ liệu sẽ ẩn đi
+
+document.addEventListener('click', function(event) {
+    const isClickInsideInput = gatauSearch.contains(event.target);
+    const isClickInsideBox = autoBox3.contains(event.target);
+    if (!isClickInsideInput && !isClickInsideBox) {
+        autoBox3.classList.remove('active');
+    }
+  });
+
+  function showBox3() {
+    const isActive = autoBox3.classList.contains('active');
+    if (!isActive) {
+        autoBox3.classList.add('active');
+    }
+  }
+  
+  gatauSearch.addEventListener('focus', showBox3);
+//
